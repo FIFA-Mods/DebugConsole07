9b0ab747ab9a662de74fff81f910f7fb7a4dc88a
dmitri-fm <dmitri.fifam@gmail.com>
2025-10-22 21:24:35 +0300

    updates

--- Main.cpp
+++ Main.cpp
@@ -41,26 +41,6 @@ public:
                     SetWindowPos(hw, HWND_TOPMOST, 20, 20, 500, 400, SWP_SHOWWINDOW | SWP_NOACTIVATE);
                     SetForegroundWindow(hw);
                 }
-                freopen_s((FILE **)stdout, "CONOUT$", "w", stdout);
-                freopen_s((FILE **)stderr, "CONOUT$", "w", stderr);
-                freopen_s((FILE **)stdin, "CONIN$", "r", stdin);
-                HANDLE hConOut = CreateFileA("CONOUT$", GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
-                HANDLE hConIn = CreateFileA("CONIN$", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
-                if (hConOut != INVALID_HANDLE_VALUE) SetStdHandle(STD_OUTPUT_HANDLE, hConOut);
-                if (hConIn != INVALID_HANDLE_VALUE) SetStdHandle(STD_INPUT_HANDLE, hConIn);
-                DWORD mode = 0;
-                HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-                if (hStdin != INVALID_HANDLE_VALUE) {
-                    GetConsoleMode(hStdin, &mode);
-                    mode &= ~ENABLE_QUICK_EDIT_MODE;
-                    mode |= (ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT);
-                    SetConsoleMode(hStdin, mode);
-                }
-
-                //patch::SetUChar(0x5F1A2F + 1, 0);
-                //patch::NopCall(0x79EA63, 2);
-                //patch::NopCall(0x79EA7B, 2);
-
                 patch::SetPointer(0x848218, DebugOutputDebugString);
                 patch::RedirectJump(0x740210, DebugPrintFormatted);
             }
